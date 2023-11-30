5d56e087c9b1d41d646d0a25fb96b6dcb901165d
LuisDiegoCF <luisdiego362@gmail.com>
2023-11-30 17:59:30 -0400

    se agrega la ruta del marcador a RouterConfig

--- NURCTFReact/src/navigation/RouterConfig.jsx
+++ NURCTFReact/src/navigation/RouterConfig.jsx
@@ -1,65 +1,73 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import BuscadorPage from "../pages/BuscadorPage";
-import { HOME_URL, LOGIN_URL, DESAFIO_CREATE_URL, REGISTER_URL, TIPO_CREATE_URL, DESAFIO_DETAIL_URL, BUSCADOR_URL,ABOUT_URL, PERFIL_URL, PISTA_CREATE_URL } from "./CONSTANTS";
+import { HOME_URL, LOGIN_URL, DESAFIO_CREATE_URL, REGISTER_URL, TIPO_CREATE_URL, DESAFIO_DETAIL_URL, BUSCADOR_URL, ABOUT_URL, PERFIL_URL, PISTA_CREATE_URL, MARCADOR_URL } from "./CONSTANTS";
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import DesafioFormPage from "../pages/desafio/DesafioFormPage";
 import DesafioDetailPage from "../pages/desafio/DesafioDetailPage";
 import TipoFormPage from "../pages/TipoFormPage";
 import PistaFormPage from "../pages/PistaFormPage";
+import MarcadorPage from "../pages/marcador/MarcadorPage";
 
 export const router = createBrowserRouter([
     {
         path: LOGIN_URL,
-        element: <LoginPage/>,
+        element: <LoginPage />,
     },
     {
         path: REGISTER_URL,
-        element: <RegisterPage/>,
+        element: <RegisterPage />,
     },
     {
         path: HOME_URL,
-        element: <HomePage/>,
+        element: <HomePage />,
     },
     {
         path: DESAFIO_CREATE_URL,
-        element: <DesafioFormPage/>,
+        element: <DesafioFormPage />,
     },
     {
         path: '/desafio/editar/:id',
-        element: <DesafioFormPage/>,
+        element: <DesafioFormPage />,
     },
     {
         path: DESAFIO_DETAIL_URL,
-        element: <DesafioDetailPage/>,
+        element: <DesafioDetailPage />,
     },
     {
         path: TIPO_CREATE_URL,
-        element: <TipoFormPage/>,
+        element: <TipoFormPage />,
     },
     {
         path: '/tipo/editar/:id',
-        element: <TipoFormPage/>,
+        element: <TipoFormPage />,
     },
     {
         path: PISTA_CREATE_URL,
-        element: <PistaFormPage/>,
+        element: <PistaFormPage />,
     },
     {
         path: '/pista/editar/:id',
-        element: <PistaFormPage/>,
+        element: <PistaFormPage />,
     },
     {
         path: PERFIL_URL,
-        element: <TipoFormPage/>,
+        element: <TipoFormPage />,
     },
     {
         path: ABOUT_URL,
-        element: <TipoFormPage/>,
+        element: <TipoFormPage />,
     },
     {
         path: BUSCADOR_URL,
-        element: <BuscadorPage/>,
-    }
+        element: <BuscadorPage />,
+    },
+
+
+
+    {
+        path: MARCADOR_URL,
+        element: <MarcadorPage />,
+    },
 ]);
