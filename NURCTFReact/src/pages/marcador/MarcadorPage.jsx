cf1307b4dcb77f8240454a9e4a697592ba17c41c
LuisDiegoCF <luisdiego362@gmail.com>
2023-11-30 17:42:58 -0400

    Creacion de la pagina marcador

---
+++ NURCTFReact/src/pages/marcador/MarcadorPage.jsx
@@ -0,0 +1,12 @@
+import Menu from "../components/Menu";
+import {  useNavigate } from "react-router-dom";
+import { getAuthToken, validateLogin } from "../utilities/TokenUtilities";
+
+const MarcadorPage = () => {
+    return ( 
+        <>
+        </>
+    );
+}
+
+export default MarcadorPage;
