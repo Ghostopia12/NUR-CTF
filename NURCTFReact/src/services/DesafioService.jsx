122459e36825ecb1c6c2d73180eff645d003e051
LuisDiegoCF <luisdiego362@gmail.com>
2023-12-01 17:07:26 -0400

    se implementa servicio de getlistadesafiosusuariosresueltos

--- NURCTFReact/src/services/DesafioService.jsx
+++ NURCTFReact/src/services/DesafioService.jsx
@@ -1,6 +1,26 @@
 import axios from "axios"
 import { BASE_URL } from "./CONSTANTS"
 
+export const getListaDesafiosUsuariosResueltos = (token) => {
+    return new Promise((resolve, reject) => {
+        axios.get(BASE_URL + "/administracion/desafio-usuario/resueltos/", {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `JWT ${token}`
+            },
+        })
+            .then((response) => {
+                console.log(response);
+                resolve(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                reject(error);
+            });
+    });
+}
+
+
 export const getListaDesafios = (token) => {
     return new Promise((resolve, reject) => {
         axios.get(BASE_URL + "/administracion/desafio/", {
