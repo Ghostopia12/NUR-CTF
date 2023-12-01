5b734268c1a7497374dd1fe73b76b9b2e6f0c67c
LuisDiegoCF <luisdiego362@gmail.com>
2023-12-01 17:06:48 -0400

    se implementa la carga de desafios resueltos

--- NURCTFReact/src/pages/desafio/DesafiosPage.jsx
+++ NURCTFReact/src/pages/desafio/DesafiosPage.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Menu from "../../components/Menu";
-import { getListaDesafios, getListaTipo } from "../../services";
-import { Card, Container } from "react-bootstrap";
+import { getListaDesafios, getListaDesafiosUsuariosResueltos, getListaTipo, postRespuestaDesafio } from "../../services";
+import { Button, Card, Container, Form, FormControl, FormGroup, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const DesafiosPage = () => {
     const navigate = useNavigate();
+    const [validated, setValidated] = useState(false);
+    const [listaResueltos, setListaResueltos] = useState([]);
     const [listaTipos, setListaTipos] = useState([]);
     const [listaDesafios, setListaDesafios] = useState([]);
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [desafioActual, setDesafioActual] = useState({});
+    const [tipoActual, setTipoActual] = useState({});
+    const [respuesta, setRespuesta] = useState('');
 
 
     useEffect(() => {
+        fetchResueltos();
         fetchDesafios();
         fetchTiposDeDesafios();
     }, []);
@@ -35,8 +42,63 @@ const DesafiosPage = () => {
             });
     };
 
-    const handleClickDesafio = (desafioId) => {
-        navigate(`/desafio/${desafioId}`);
+    const fetchResueltos = () => {
+        getListaDesafiosUsuariosResueltos(localStorage.getItem('token'))
+            .then((data) => {
+                setListaResueltos(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching desafios:', error);
+            });
+    };
+
+    const abrirModal = () => {
+        setMostrarModal(true);
+        setValidated(false);
+    };
+
+    const cerrarModal = () => {
+        setMostrarModal(false);
+        setValidated(false);
+    };
+
+    const handleClickDesafio = (desafio, tipo) => {
+        // navigate(`/desafio/${desafioId}`);
+        abrirModal();
+        setDesafioActual(desafio);
+        setTipoActual(tipo);
+    }
+
+    const onSubmitFlag = (e) => {
+        console.log(respuesta);
+        const form = e.currentTarget;
+        let isValid = form.checkValidity();
+        e.preventDefault();
+        e.stopPropagation();
+        setValidated(true);
+        if (!isValid) return;
+        saveRespuesta();
+    }
+
+    const saveRespuesta = () => {
+        "desafio_id": [
+            "This field is required."
+        ],
+        "usuario_id": [
+            "This field is required."
+        ],
+        "intento": [
+            "This field is required."
+        ]
+        postRespuestaDesafio(localStorage.getItem('token'), desafioActual.id, respuesta)
+            .then((data) => {
+                console.log(data);
+                fetchResueltos();
+                cerrarModal();
+            })
+            .catch((error) => {
+                console.error('Error fetching desafios:', error);
+            });
     }
 
     return (
@@ -47,18 +109,19 @@ const DesafiosPage = () => {
                 <Container>
                     {listaTipos.map((tipo) => (
                         <div key={tipo.id}>
-                            <Card.Header>{tipo.nombre} {tipo.id}</Card.Header>
+                            <Card.Header className="text-center">{tipo.nombre}</Card.Header>
                             <div className="row justify-content-center">
                                 {listaDesafios.map((desafio) => (
                                     <React.Fragment key={desafio.id}>
                                         {desafio.tipo.id === tipo.id && (
                                             <Card
-                                                className="col-md-3 mb-2 m-3"
-                                                onClick={() => handleClickDesafio(desafio.id)}
+                                                className={`col-md-3 mb-2 m-3 ${listaResueltos.some(item => item.desafio_u === desafio.id) ? 'bg-success' : ''}`}
+                                                onClick={() => handleClickDesafio(desafio, tipo)}
                                             >
                                                 <Card.Body className="text-center">
                                                     <Card.Title>{desafio.titulo}</Card.Title>
                                                     <Card.Text>{desafio.puntos}</Card.Text>
+                                                    {/* <Card.Text>{desafio.id}</Card.Text> */}
                                                 </Card.Body>
                                             </Card>
                                         )}
@@ -69,8 +132,57 @@ const DesafiosPage = () => {
                     ))}
                 </Container>
             </Card>
+
+            <Modal show={mostrarModal} onHide={cerrarModal}>
+                <Form noValidate onSubmit={onSubmitFlag} validated={validated}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{tipoActual.nombre}</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body className="text-center">
+                        <h1>{desafioActual.titulo}</h1>
+                        <h4>{desafioActual.puntos}</h4>
+                        <h7>
+                            {desafioActual.descripcion}
+                        </h7>
+                        {desafioActual.archivo && !desafioActual.archivo.includes("null") && (
+                            <>
+                                <br />
+                                <a
+                                    href={desafioActual.archivo}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    download
+                                >
+                                    Descargar Archivo
+                                </a>
+                            </>
+                        )}
+                        <br />
+                        <br />
+
+                        <FormGroup>
+                            <FormControl value={respuesta} required placeholder="NurCTF{flag}"
+                                onChange={(e) => {
+                                    setRespuesta(e.target.value);
+                                }} />
+                            <Form.Control.Feedback type="invalid">Insert flag</Form.Control.Feedback>
+                        </FormGroup>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" variant="primary">
+                            Submit
+                        </Button>
+                        <Button variant="secondary" onClick={cerrarModal}>
+                            Cerrar
+                        </Button>
+                        {/* Puedes agregar más botones según tus necesidades */}
+                    </Modal.Footer>
+                </Form>
+            </Modal>
         </>
     );
+
 };
 
 export default DesafiosPage;
